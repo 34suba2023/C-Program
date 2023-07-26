f8b68e9b3b97bcacef86e0c389fbb4f7931f3e96
SUBASREE K <124561940+34suba2023@users.noreply.github.com>
2023-07-26 07:03:42 +0530

    Add files via upload

---
+++ Method 1 Using Brute Force to check whether a given integer is Positive or Negative.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    if(a>0){
+        printf("Number is positive");
+    }
+    else if(a<0){
+        printf("Number is Negative");
+    }
+    else{
+        printf("Zero");
+    }
+    return 0;
+}
---
+++ Method 3 Using Ternary Operators to check whether a number is Positive or Negative.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int num=10;
+
+    if (num==0)
+        printf("Zero");
+    else(num>0)? printf("Positive"):printf("Negative");
+
+
+
+}
